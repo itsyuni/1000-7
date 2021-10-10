3fbec8abd6fa9b8b282cf278eb45de13eaa44ff3
Vadim Korovin <smartdroidyt@gmail.com>
2021-10-10 21:56:07 +0300

    лучший код в мире v1

---
+++ deadinside.py
@@ -0,0 +1,13 @@
+# Дединсайдам привет, остальным соболезную
+# by https://yuni.su
+
+import time
+
+x = 1007 - 7
+
+while x > 7:
+     print(f"{x} - 7 = {x-7}")
+     x -= 7
+     time.sleep(0.5)
+
+print('you deadinside xd')
