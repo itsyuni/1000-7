1ed9f02ae0be8661ab225ba80227231d2e6c4b4b
Vadim Korovin <smartdroidyt@gmail.com>
2021-10-10 22:31:41 +0300

    жееесть теперь и на паскале

---
+++ deadinside.pas
@@ -0,0 +1,16 @@
+// дединсайдам привет, остальным соболезную
+// by https://yuni.su
+
+var dead: integer;
+begin
+   dead:=1000;
+   while (dead > 7) do
+   begin
+   writeln(У меня нет проблем)
+   writeln(Кроме моей башки)
+   writeln(Прости, я умер)
+   writeln(dead, ' - 7 = ', dead-7)
+   dead:=dead-7;
+   writeln(Теперь вы дединсайдик)
+   end:
+end.   
