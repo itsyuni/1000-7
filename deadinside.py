5795d1e44bbb61b91937633a81f70960fb9c82db
Vadim Korovin <smartdroidyt@gmail.com>
2021-10-10 22:00:11 +0300

    Мама, я программист

--- deadinside.py
+++ deadinside.py
@@ -5,9 +5,17 @@ import time
 
 x = 1007 - 7
 
+time.sleep(0.2)
+print('У меня нет проблем')
+time.sleep(0.3)
+print('Кроме моей башки')
+time.sleep(0.3)
+print('Прости, я умер')
+time.sleep(0.5)
+
 while x > 7:
      print(f"{x} - 7 = {x-7}")
      x -= 7
      time.sleep(0.5)
 
-print('you deadinside xd')
+print('Теперь вы дединсайдик')
