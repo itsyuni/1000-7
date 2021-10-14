79aceb155c949595ad701ac6a6cc42281aa01112
timofej673 <54764164+timofej673@users.noreply.github.com>
2021-10-14 14:45:47 +0400

    Исправляем смешные ошибки

--- deadinside.pas
+++ deadinside.pas
@@ -5,13 +5,14 @@
 var dead: integer;
 begin
    dead:=1000;
+   writeln('У меня нет проблем');
+   writeln('Кроме моей башки');
+   writeln('Прости, я умер');
    while (dead > 7) do
    begin
-   writeln("У меня нет проблем")
-   writeln("Кроме моей башки")
-   writeln("Прости, я умер")
-   writeln(dead, ' - 7 = ', dead-7)
-   dead:=dead-7;
-   writeln("Теперь вы дединсайдик")
-   end:
+     writeln(dead, ' - 7 = ', dead-7);
+     dead:=dead-7;
+     Sleep(1000);
+   end;
+   writeln('Теперь вы дединсайдик');
 end.   
